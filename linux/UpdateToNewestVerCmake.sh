b4c7f84ecfaefd429127f7ab107816e0ebdf17f6
Bill <read0651@hotmail.com>
2022-01-21 16:26:39 +0800

    2022.01.21 sc.lin New: Added update cmake shell script in arm linux.

---
+++ linux/UpdateToNewestVerCmake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is going to remove cmake in OS and install newest version cmake.
+
+apt remove cmake
+
+sudo apt install -y libssl-dev openssl1.0
+# For building cmake-gui
+sudo apt install -y qt5-default
+
+wget -c --show-progress https://github.com/Kitware/CMake/releases/download/v3.22.1/cmake-3.22.1.tar.gz
+tar -xvf cmake*.tar.gz
+rm cmake*.tar.gz
+
+cd cmake-3.22.1
+./configure
+sudo make install
+
+cmake --version
